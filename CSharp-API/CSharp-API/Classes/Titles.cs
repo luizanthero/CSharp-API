b915c060a307c62c0a70d77c4b2e3d53197fc43f
Luiz Anthero <luis.anthero@hotmail.com>
2019-03-11 16:28:12 -0300

    Classe Titles

---
+++ CSharp-API/CSharp-API/Classes/Titles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_API.Classes
+{
+    class Titles
+    {
+        public string en { get; set; }
+        public string en_jp { get; set; }
+        public string en_us { get; set; }
+        public string ja_jp { get; set; }
+    }
+}

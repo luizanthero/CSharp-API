69b7d2c901a9b2ef442c32f5390b8450b7f1ef47
Luiz Anthero <luis.anthero@hotmail.com>
2019-03-11 16:01:17 -0300

    Classe Anime e Data

---
+++ CSharp-API/CSharp-API/Classes/Anime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_API.Classes
+{
+    class Anime
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+    }
+}

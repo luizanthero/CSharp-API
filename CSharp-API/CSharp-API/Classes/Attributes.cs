e33f25b4be49a38bd5fec1edd290f69139aa45c4
Luiz Anthero <luis.anthero@hotmail.com>
2019-03-11 16:30:22 -0300

    Classe Attributes

---
+++ CSharp-API/CSharp-API/Classes/Attributes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_API.Classes
+{
+    class Attributes
+    {
+        public Titles titles { get; set; }
+        public Poster posterImage { get; set; }
+        public string canonicalTitle { get; set; }
+        public string synopsis { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public string ageRatingGuide { get; set; }
+        public string youtubeVideoId { get; set; }
+        public string episodeCount { get; set; }
+        public string episodeLength { get; set; }
+    }
+}

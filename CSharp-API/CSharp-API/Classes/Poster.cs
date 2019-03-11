3ceca5d3abd99061bc0114307ba6094daeeb09c0
Luiz Anthero <luis.anthero@hotmail.com>
2019-03-11 16:29:15 -0300

    Classe Poster

---
+++ CSharp-API/CSharp-API/Classes/Poster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_API.Classes
+{
+    class Poster
+    {
+        public string tiny { get; set; }
+        public string small { get; set; }
+        public string medium { get; set; }
+        public string large { get; set; }
+        public string original { get; set; }
+    }
+}
